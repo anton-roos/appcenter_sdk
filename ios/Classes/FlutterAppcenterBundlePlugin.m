931953f7be60fc5918f888414b6a70a9c1ee06c2
Anton Roos <antonroos992@gmail.com>
2021-07-30 01:13:36 +0200

    init

---
+++ ios/Classes/FlutterAppcenterBundlePlugin.m
@@ -0,0 +1,15 @@
+#import "FlutterAppcenterBundlePlugin.h"
+#if __has_include(<appcenter_sdk/appcenter_sdk-Swift.h>)
+#import <appcenter_sdk/appcenter_sdk-Swift.h>
+#else
+// Support project import fallback if the generated compatibility header
+// is not copied when this plugin is created as a library.
+// https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
+#import "appcenter_sdk-Swift.h"
+#endif
+
+@implementation FlutterAppcenterBundlePlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  [SwiftFlutterAppcenterBundlePlugin registerWithRegistrar:registrar];
+}
+@end
